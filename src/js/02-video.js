8366330627e716f4ecff68d54cb3e538098b5b16
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-09 15:26:19 +0200

    throttle to do

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 import '../css/common.css';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+//get controls
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+//resume player
+resumePlayerFromLastTime(player, localStorage.getItem('videoplayer-current-time'));
+
+//upd last time
+
+player.on('timeupdate', ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+});
+
+// player.on('timeupdate', ({ seconds }) => {
+//   throttle(localStorage.setItem('videoplayer-current-time', seconds), 2000);
+// });
+
+function resumePlayerFromLastTime(currentPlayer, currentTime) {
+  if (currentTime) currentPlayer.setCurrentTime(currentTime);
+}
