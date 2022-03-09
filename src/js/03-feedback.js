0d7c7a757b2cc4ecb3f3cc12b909847bf37d92ca
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-09 22:10:59 +0200

    task 3 began

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
-
+//get controls
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', event => {
+  const formData = {
+    email: event.currentTarget.elements.email.value,
+    message: event.currentTarget.elements.message.value,
+  };
+  //make JSON and set it to lStorage
+  //   localStorage.setItem();
+  console.log(formData);
+});
